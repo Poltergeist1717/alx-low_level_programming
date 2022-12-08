1e2956b70f215d136ae1c74d855d7beefc0cf963
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-08 08:49:07 -0800

    ALX low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * the program prints the phrase (Programming is like building a multilingual puzzle)
- * it returns 0
- * */
+ * the program prints the phrase: Programming is like building a multilingual puzzle
+ * it returns: 0
+ */
 int main(void) 
 {puts("\"Programming is like building a multilingual puzzle");
 	return (0);
