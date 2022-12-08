d50716644c5d7e318d3497844c0fefa63ac16578
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-08 09:14:12 -0800

    ALX low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * this program prints the phrase: with proper grammar, but the outcome is a piece of art
+ * returns 0
+ */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
