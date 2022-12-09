84abaf528fc2499b658ec665eeb6ee0559fcba32
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 13:10:45 -0800

    if else while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int last_digit;
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
-	printf("Last digit of 98 is %d and  is greater than 5\n", last_digit);
+	printf("Last digit of 98 is %d and is greater than 5\n", last_digit);
 	}
 	else if (last_digit == 0)
 	{
