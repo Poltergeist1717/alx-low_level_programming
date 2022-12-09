52e5e9c5cd1db94b6883e018b28189a12c453da9
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 15:02:57 -0800

    if else while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main: program entry 
+  * Description: print all single digit number of base 10
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a=0, a<=10, a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return(0);
+}
