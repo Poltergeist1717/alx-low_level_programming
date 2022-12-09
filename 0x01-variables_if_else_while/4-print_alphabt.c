38c12a19a0f8fe1556720edf9737734b2a67e817
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 13:38:58 -0800

    if else while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
   * main - entry
-  * Decription - prints alphabets except e and q
+  * Description - prints alphabets except e and q
   * Return - returns 0
   */
 
 int main(void)
 {
 	char letter;
-	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 	if (letter == 'e' && letter == 'q')
 	{
 		continue;
-		putchar(letter);
+	putchar(letter);
 	}
 	}
 	return (0);
