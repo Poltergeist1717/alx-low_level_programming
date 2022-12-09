b570b5b199301e8d28e4f116744896cd168f6aad
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 15:50:49 -0800

    if else while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
-  * main: entry point
+  *main: entry point
   * Description: print numbers of base 10
   * Return: always 0
   */
@@ -12,9 +12,9 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		putchar(a);
+		putchar(a + '0');
 	}
 	putchar ('\n');
 	return (0);
