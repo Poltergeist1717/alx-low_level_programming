4d272dffe2268b7e3aa8f858e15155424d7c2e25
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 06:52:15 -0800

    if else while

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - entry
+  * Decription - prints alphabets except e and q
+  * Return - returns 0
+  */
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = a; letter <= z; letter++)
+	{
+	if (letter != e && letter != q)
+	putchar(letter);
+	}
+	putchar(n);
+	return (0);
+}
