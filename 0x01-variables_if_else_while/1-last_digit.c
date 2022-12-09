98361514aa5962460e9702fc1f95c73a7efbf9bc
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-09 03:00:30 -0800

    if else while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+		if (n > 5)
+		{printf("%d is greater than 5 ", n)
+		}
+		if (n == 0)
+		{printf("%d is 0 ", n)
+		}
+		if (n < 6)
+		{printf("%d and is less than 6 and 0", n)
+		}
+		return (0);
+}
