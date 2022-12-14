b2a7ac9a40f5442a6eddca22231c662078ada7bf
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-13 17:32:10 -0800

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * Description: a function that prints the alphabet,
+  * in lowecase followed by a new line
+  * Return: void
+  */
+
+void print_alphabet(void)
+
+{
+
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+}
