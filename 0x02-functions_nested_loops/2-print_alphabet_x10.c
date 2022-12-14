3661a2b0b4cf4aa74a026aee06707e4711fe19fb
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-13 23:12:13 -0800

    Nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+   * print_alphabet_x10 - function
+   * Description: print alphabets in lowercase x10
+   * Return: void
+   */
+void print_alphabet_x10(void)
+{
+	int cnt;
+	char i;
+
+	for (cnt = 1; cnt <= 10; cnt++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	}
+}
