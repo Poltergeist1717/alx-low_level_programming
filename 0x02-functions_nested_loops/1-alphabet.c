3094c9dc4ae81f7b1114d28d368b0e541fa55b88
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-13 23:38:38 -0800

    Nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
   * main - Entry point
   * Description: a function that prints the alphabet,
   * in lowecase followed by a new line
-  * Return: void
+  * Return: returns  void
   */
 
 void print_alphabet(void)
