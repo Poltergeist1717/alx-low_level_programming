f28f428e46b09d1206c4c6abab22724edffd5b64
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-13 16:45:50 -0800

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main: entry point
+  * Description: prints _putchar
+  * Return: always 0
+  */
+
+int main(void)
+{
+	char c = _putchar
+
+	printf("%c", c);
+	return (0);
+}
