a034839b79a0f4a7d9388d3231ebef93ec0a42fb
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-13 22:57:06 -0800

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-  * main: entry point
+  * main - entry point
   * Description: prints _putchar
   * Return: always 0
   */
@@ -11,6 +11,7 @@ int main(void)
 {
 	char msg[] = "_putchar\n";
 	int i;
+
 	for (i = 0; i < 13; i++)
 	{
 	if (msg[i] == '\n')
