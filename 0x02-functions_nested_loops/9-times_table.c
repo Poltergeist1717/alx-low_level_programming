fe6f90abcca9e82bdda3918e405a7a32c036df50
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-14 01:55:10 -0800

    Nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,22 +11,18 @@ void times_table(void)
 {
 
 	int row, column, product, tens, ones;
-	
-	for (row = 0; row <= 9; row++)
 
+	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
-
 		{
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
-
 			if (column == 0)
 			{
 				_putchar('0');
 			}
-
 			else if (product < 10)
 			{
 				_putchar(',');
@@ -34,7 +30,6 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(ones + '0');
 			}
-
 			else
 			{
 				_putchar(',');
