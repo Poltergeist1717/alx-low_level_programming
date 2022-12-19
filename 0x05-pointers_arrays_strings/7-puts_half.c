fbe67b87fbbd36328bf84dccb05888a3f5e055d5
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-19 15:47:45 -0800

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
 
   * puts_half - prints half of a string
@@ -18,40 +16,24 @@ void puts_half(char *str)
 
 	int len, n, i;
 
-
-
 	len = 0;
-
-
 
 	while (str[len] != '\0')
 
 	{
-
 		len++;
-
 	}
-
-
-
 	if (len % 2 == 0)
-
 	{
-
 		for (i = len / 2; str[i] != '\0'; i++)
-
-		{
-
-			_putchar(str[i]);
-
-		}
+	{
+		_putchar(str[i]);
+	}
 
 	} else if (len % 2)
 
 	{
-
-		for (n = (len - 1) / 2; n < len - 1; n++)
-
+	for (n = (len - 1) / 2; n < len - 1; n++)
 		{
 
 			_putchar(str[n + 1]);
