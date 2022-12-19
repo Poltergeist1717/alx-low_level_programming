3865c4667a6104562768ef9d5e3dfaa76bb98bcd
Olamide Yakub Lateef <Lateefolamide5595@gmail.com>
2022-12-19 14:55:39 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+  * swap_int - swap values of pointers
+  * @a: first pointer
+  * @b: second pointer
+  * Return: Nothing
+  */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
